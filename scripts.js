4ab46d51af2abf00b4563926bf5ada4433abeca2
Albert Arrieta <albert.arrieta@uleth.ca>
2023-11-02 17:14:35 -0600

    multiple words

--- scripts.js
+++ scripts.js
@@ -1,34 +1,38 @@
-let word = 'the';
+// contentScript.js
+let words = ['the', 'how', 'spider']; // The words to search for
 
 let observer = new MutationObserver(function(mutations) {
   // Select all the video title elements
   let titles = document.querySelectorAll('ytd-rich-item-renderer #video-title');
 
-  // Create a regular expression to match the word case-insensitively
-  let regex = new RegExp(`\\b${word}\\b`, 'i');
-
   // Loop through each title
   titles.forEach(function(title) {
-    // Check if the title includes the word
-    if (regex.test(title.textContent)) {
-      // Get the parent "ytd-rich-item-renderer" element
-      let parent = title.closest('ytd-rich-item-renderer');
+    // Loop through each word
+    words.forEach(function(word) {
+      // Create a regular expression to match the word case-insensitively
+      let regex = new RegExp(`\\b${word}\\b`, 'i');
 
-      // Add new div to block content.
-      let newDiv = document.createElement('div');
-      newDiv.textContent = 'Spoiler Guard blocked the following word: "' + word + '"';
+      // Check if the title includes the word
+      if (regex.test(title.textContent)) {
+        // Get the parent "ytd-rich-item-renderer" element
+        let parent = title.closest('ytd-rich-item-renderer');
 
-      newDiv.style.backgroundColor = 'red';
-      newDiv.style.width = parent.offsetWidth + 'px';
-      newDiv.style.height = parent.offsetHeight + 'px';
-      newDiv.style.color = 'white';
-      newDiv.style.fontSize = '20px';
-      newDiv.style.borderRadius = '5px';
-      newDiv.style.margin = '2px';
-      newDiv.style.padding = '5px';
+        // Add new div to block content.
+        let newDiv = document.createElement('div');
+        newDiv.textContent = 'Spoiler Guard blocked the following word: "' + word + '"';
 
-      parent.parentNode.replaceChild(newDiv, parent);
-    }
+        newDiv.style.backgroundColor = 'red';
+        newDiv.style.width = parent.offsetWidth + 'px';
+        newDiv.style.height = parent.offsetHeight + 'px';
+        newDiv.style.color = 'white';
+        newDiv.style.fontSize = '20px';
+        newDiv.style.borderRadius = '5px';
+        newDiv.style.margin = '2px';
+        newDiv.style.padding = '5px';
+
+        parent.parentNode.replaceChild(newDiv, parent);
+      }
+    });
   });
 });
 
